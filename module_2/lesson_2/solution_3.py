9e5a818128c15c2c028a0af06e95a3bbbed5ef6c
kaievil <natasha20001lol@gmail.com>
2024-05-04 18:10:14 +0300

    finished solution3

---
+++ module_2/lesson_2/solution_3.py
@@ -0,0 +1,6 @@
+print("Введите 3 числа")
+number_1=int(input())
+number_2=int(input())
+number_3=int(input())
+#arfm=(number_1 + number_2 + number_3)/ 3 
+print ("Среднее арифметическое:", (number_1 + number_2 + number_3)/ 3)
